0b7a552f692b92a6cc7db4a45399d273f97617c5
shreyas-urankar <urankarshreyas@gmail.com>
2024-10-12 21:38:21 +0530

    Updated Functions.js with recent changes

--- Functions.js
+++ Functions.js
@@ -63,7 +63,7 @@
 //     document.getElementById("demo").innerHTML += val + "<br>"; 
 // });
 
-// //Uding arroe function in array:
+// //Using arrow function in array:
 // arr.forEach((val)=>{
 //     document.getElementById("demo").innerHTML += val + "<br>"; 
     
@@ -71,7 +71,7 @@
 
 //.......................................................................
 
-// QUE PRACTICE:For given array of numbers printed square of each value using the for each loop.
+// QUE PRACTICE:For given array of numbers print square of each value using the for each loop.
 
 // let nums=[1,2,3,4,5,6];
 // nums.forEach((num)=>{
@@ -109,14 +109,50 @@
 
 //...........................................................
 // 3)Reduce Method()
-let arr=[1,2,3,5,6,7];
-const output=arr.reduce((res, curr)=>{
-    return res + curr;
-});
-document.getElementById("demo").innerHTML=output;
 
-// If previous value is greater than the current then return previous or return current(MAIN QUE IS TO PRINT THE LARGEST NUMBER IN THE ARRAY.)
-const output1=arr.reduce((prev, current)=>{
-    return prev>current ? prev:curr;
-});
-document.getElementById("demo").innerHTML=output1;
+// let arr=[1,2,3,5,6,7];
+// const output=arr.reduce((res, curr)=>{
+//     return res + curr;
+// });
+// document.getElementById("demo").innerHTML=output;
+
+// // If previous value is greater than the current then return previous or return current(MAIN QUE IS TO PRINT THE LARGEST NUMBER IN THE ARRAY.)
+// const output1=arr.reduce((prev, current)=>{
+//     return prev>current ? prev:curr;
+// });
+// document.getElementById("demo").innerHTML=output1;
+
+//.........................................................................
+//We are given array of marks of students. Filter out of the marks of students that scored 90+.
+
+// let marks = [50,98,85,73,62,95,94,68,100,91] 
+// let toppers = marks.filter((val) =>{
+//     return val > 90;
+// })
+// document.getElementById("demo").innerHTML = "The toppers are: " + toppers.join(", ");
+
+// ...................................................................................................
+
+// Take a number n as input from user create an array of numbers from one to N. Use the reduce method to calculate sum of all numbers in the array. You. use the reduce method to calculate product of all numbers in the array.
+
+let n = prompt("Enter a number:");
+
+let array = [];
+for (let i=1; i<=n; i++)
+{
+    array[i-1] = i;
+}
+
+document.getElementById("demo").innerHTML = "Array:" + array.join(",");
+
+let sum = array.reduce((res, curr) => {
+    return res + curr;
+})
+
+document.getElementById("demo").innerHTML += "<br>Sum of Array:" + sum;
+
+let factorial = array.reduce((res, curr) => {
+    return res * curr;
+})
+
+document.getElementById("demo").innerHTML += "<br>Factorial of Array:" + factorial;
